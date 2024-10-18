0251f9db5df5c746418b7bf56d47bb840aa6eb99
Roi_sysmique <elhechmihamza@gmail.com>
2024-10-18 12:27:52 +0100

    'Main-Bug-Fixed'

--- main.py
+++ main.py
@@ -2,38 +2,18 @@ import pygame
 
 pygame.init()
 
-game_env = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
-
-SCREEN_HEIGHT = 400
-SCREEN_WIDTH = 400
-
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN_LENGTH = 400
+cell_length = SCREEN_LENGTH/20
+game_env = [0 for _ in range(int(cell_length) + 10) for _ in range(int(cell_length) + 10)]
+for _ in game_env:
+    print(_)
+SCREEN = pygame.display.set_mode((SCREEN_LENGTH, SCREEN_LENGTH))
 CLOCK = pygame.time.Clock()
 FPS = 300
 setup_mode = True
 game_mode = False
 click_pos = None
+generations = 0
 
 
 class Cell(pygame.sprite.Sprite):
@@ -53,59 +33,52 @@ class Cell(pygame.sprite.Sprite):
                                    (-1, -1), (-1, 1), (1, -1), (1, 1)]
 
         if self.row == 0:
-            self.safe_remove((-1, 0))
-            self.safe_remove((-1, -1))
-            self.safe_remove((-1, 1))
+            self.neighbor_positions.remove((-1, 0))
+            self.neighbor_positions.remove((-1, -1))
+            self.neighbor_positions.remove((-1, 1))
             if self.col == 0:
-                self.safe_remove((0, -1))
-                self.safe_remove((1, -1))
+                self.neighbor_positions.remove((0, -1))
+                self.neighbor_positions.remove((1, -1))
             elif self.col == len(game_env) - 1:
-                self.safe_remove((0, -1))
-                self.safe_remove((1, -1))
+                self.neighbor_positions.remove((0, 1))
+                self.neighbor_positions.remove((1, 1))
         elif self.row == len(game_env) - 1:
-            self.safe_remove((1, 0))
-            self.safe_remove((1, -1))
-            self.safe_remove((1, 1))
+            self.neighbor_positions.remove((1, 0))
+            self.neighbor_positions.remove((1, -1))
+            self.neighbor_positions.remove((1, 1))
             if self.col == 0:
-                self.safe_remove((0, -1))
-                self.safe_remove((-1, -1))
+                self.neighbor_positions.remove((0, -1))
+                self.neighbor_positions.remove((-1, -1))
             if self.col == len(game_env) - 1:
-                self.safe_remove((-1, 1))
-                self.safe_remove((0, 1))
+                self.neighbor_positions.remove((-1, 1))
+                self.neighbor_positions.remove((0, 1))
         elif self.col == 0 and (self.row != 0 and self.row != len(game_env) - 1):
-            self.safe_remove((1, -1))
-            self.safe_remove((0, -1))
-            self.safe_remove((-1, -1))
+            self.neighbor_positions.remove((1, 1))
+            self.neighbor_positions.remove((0, 1))
+            self.neighbor_positions.remove((-1, -1))
         elif self.col == len(game_env) - 1 and (self.row != 0 and self.row != len(game_env) - 1):
-            self.safe_remove((1, -1))
-            self.safe_remove((0, -1))
-            self.safe_remove((-1, -1))
+            self.neighbor_positions.remove((1, 1))
+            self.neighbor_positions.remove((0, 1))
+            self.neighbor_positions.remove((-1, 1))
 
-        self.cells_to_check = []
-        for i in self.neighbor_positions:
-            new_row = self.row + i[0]
-            new_col = self.col + i[1]
-            if 0 <= new_row < len(game_env) and 0 <= new_col < len(game_env[0]):
-                self.cells_to_check.append(game_env[new_row][new_col])
-
-    def safe_remove(self, pos):
-        if pos in self.neighbor_positions:
-            self.neighbor_positions.remove(pos)
+        self.cells_to_check = None
 
     def update(self, last_click_pos):
-        global game_env, FPS
+        global game_env, FPS, generations
+
         self.cells_to_check = []
-        for i in self.neighbor_positions:
-            new_row = self.row + i[0]
-            new_col = self.col + i[1]
-            if 0 <= new_row < len(game_env) and 0 <= new_col < len(game_env[0]):
-                self.cells_to_check.append(game_env[new_row][new_col])
+        for _ in self.neighbor_positions:
+            new_row = self.row + _[0]
+            new_col = self.col + _[1]
+            self.cells_to_check.append(game_env[new_row][new_col])
         if self.status:
             self.image.fill('white')
         else:
             self.image.fill('black')
+
         if setup_mode and type(last_click_pos) == tuple and last_click_pos != self.last_click_pos:
             FPS = 300
+            generations = 0
             self.last_click_pos = last_click_pos
             if self.rect.collidepoint(last_click_pos) and not self.status:
                 self.status = True
@@ -118,23 +91,17 @@ class Cell(pygame.sprite.Sprite):
             FPS = 10
             if not self.status and self.cells_to_check.count(1) == 3:
                 self.status = True
-                game_env[self.row][self.col] = 1
-                print(f"live:{self.cells_to_check.count(1)}")
             elif self.status and self.cells_to_check.count(1) < 2 or self.cells_to_check.count(1) > 3:
                 self.status = False
-                game_env[self.row][self.col] = 0
-                print(f"Die:{self.cells_to_check.count(1)}")
             elif self.status and self.cells_to_check.count(1) == 2 or self.cells_to_check.count(1) == 3:
                 self.status = True
-                game_env[self.row][self.col] = 1
-                print(f"Die:{self.cells_to_check.count(1)}")
 
 
 def lines(surface_width, game_environment):
     width_square = len(game_environment)
-    for i in range(width_square, surface_width + 1, width_square):
-        pygame.draw.line(SCREEN, 'grey', (i, 0), (i, surface_width))
-        pygame.draw.line(SCREEN, 'grey', (0, i), (surface_width, i))
+    for _ in range(width_square, surface_width + 1, width_square):
+        pygame.draw.line(SCREEN, 'grey', (_, 0), (_, surface_width))
+        pygame.draw.line(SCREEN, 'grey', (0, _), (surface_width, i))
 
 
 cells = pygame.sprite.Group()
@@ -158,9 +125,15 @@ while True:
             elif event.key == pygame.K_ESCAPE:
                 setup_mode = True
                 game_mode = False
-
     cells.draw(SCREEN)
     cells.update(click_pos)
-    lines(SCREEN_WIDTH, game_env)
+    if game_mode:
+        generations += 1
+        for i in cells:
+            if i.status:
+                game_env[i.row][i.col] = 1
+            elif not i.status:
+                game_env[i.row][i.col] = 0
+    lines(SCREEN_LENGTH, game_env)
     pygame.display.update()
     CLOCK.tick(FPS)
